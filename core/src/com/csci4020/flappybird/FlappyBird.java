729baf7064e1fc88c3a3d60fb11a90ead0baf20e
Brian Lake <bnlake@gmail.com>
2019-04-24 21:20:11 -0500

    Set up blank libgx methods

--- core/src/com/csci4020/flappybird/FlappyBird.java
+++ core/src/com/csci4020/flappybird/FlappyBird.java
@@ -1,235 +1,30 @@
 package com.csci4020.flappybird;
 
+
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.BitmapFont;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
-import com.badlogic.gdx.math.Circle;
-import com.badlogic.gdx.math.Intersector;
-import com.badlogic.gdx.math.Rectangle;
 
-import org.w3c.dom.css.Rect;
-
-import java.util.Random;
-
-public class FlappyBird extends ApplicationAdapter {
-	// lets us render textures
-    SpriteBatch batch;
-	Texture background;
-	Texture[] birds;
-	Texture gameover;
-
-    Random randomGenerator;
-
-	// keep track of which bird we'd like to display
-	int flapState = 0;
-	float birdY = 0;
-	float velocity = 0;
-	Circle birdCircle;
-
-	// keeps track of the state of the game
-	int gameState = 0;
-	float gravity = 2;
-
-	Texture topTube;
-	Texture bottomTube;
-
-	// distance between the pipes
-	float gap = 400;
-	// to set different sizes of the tubes
-	float maxTubeOffset;
-	float tubeVelocity = 4;
-	int numberOfTubes = 4;
-    float[] tubeX = new float[numberOfTubes];
-    float[] tubeOffset = new float[numberOfTubes];
-	float distanceBetweenTubes;
-	Rectangle[] topTubeRectangles;
-	Rectangle[] bottomTubeRectangles;
-
-	int score = 0;
-	int scoringTube = 0;
-    BitmapFont font;
-
-	/**
-	 * Runs when app begins
-	 */
+public class FlappyBird extends ApplicationAdapter
+{
 	@Override
-	public void create () {
-	    // manages displaying the sprites
-		batch = new SpriteBatch();
-        background = new Texture(("bg.png"));
-        gameover = new Texture("gameover.png");
-        birdCircle = new Circle();
-        font = new BitmapFont();
-        font.setColor(Color.WHITE);
-        font.getData().setScale(10);
+	public void create()
+	{
 
-        birds = new Texture[2];
-        birds[0] = new Texture("bird.png");
-        birds[1] = new Texture("bird2.png");
-
-        topTube = new Texture("toptube.png");
-        bottomTube = new Texture("bottomtube.png");
-        maxTubeOffset = Gdx.graphics.getHeight() / 2 - gap / 2 - 100;
-        randomGenerator = new Random();
-        distanceBetweenTubes = Gdx.graphics.getWidth() / 2;
-        topTubeRectangles = new Rectangle[numberOfTubes];
-        bottomTubeRectangles = new Rectangle[numberOfTubes];
-
-        startGame();
 	}
 
-	public void startGame() {
+	public void startGame()
+	{
 
-        birdY = Gdx.graphics.getHeight() / 2 - birds[0].getHeight() / 2;
-
-        // set x coordinate and offset
-        for (int i = 0; i < numberOfTubes; i++) {
-
-            tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
-
-            // resets tubeX each tap
-            tubeX[i] = Gdx.graphics.getWidth() / 2 - topTube.getWidth() / 2 + Gdx.graphics.getWidth() + i * distanceBetweenTubes;
-
-            topTubeRectangles[i] = new Rectangle();
-            bottomTubeRectangles[i] = new Rectangle();
-        }
-    }
-
-	/**
-	 * Runs continuously while app is running
-	 */
-	@Override
-	public void render () {
-
-	    // Draw background first so it doesn't cover up the bird and pipes
-        // start displaying sprits
-        batch.begin();
-        // take up the whole screen
-        batch.draw(background, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-
-        if (gameState == 1) {
-
-            if (tubeX[scoringTube] < Gdx.graphics.getWidth()) {
-
-                score++;
-
-                Gdx.app.log("Score", String.valueOf(score));
-
-                if (scoringTube < numberOfTubes - 1) {
-
-                    scoringTube++;
-                }
-                else {
-
-                    scoringTube = 0;
-                }
-            }
-
-            // moves bird up when tapped
-            if (Gdx.input.justTouched()) {
-
-                velocity = -30;
-            }
-
-            for (int i = 0; i < numberOfTubes; i++) {
-
-                // when tube is off the edge of screen shift tubes to the right
-                if (tubeX[i] < - topTube.getWidth()) {
-
-                    tubeX[i] += numberOfTubes * distanceBetweenTubes;
-                    // 0.5 because we want 0.5 or -0.5 since we're working in center of the screen
-                    // used to shift the pipes up and down. half of height minus the gap - 100 for each pipe
-                    tubeOffset[i] = (randomGenerator.nextFloat() - 0.5f) * (Gdx.graphics.getHeight() - gap - 200);
-                }
-                else {
-
-                    tubeX[i] -= tubeVelocity;
-                }
-
-                batch.draw(topTube, tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i]);
-                batch.draw(bottomTube, tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i]);
-
-                // should overlap the existing tubes
-                topTubeRectangles[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 + gap / 2 + tubeOffset[i], topTube.getWidth(), topTube.getHeight());
-                bottomTubeRectangles[i] = new Rectangle(tubeX[i], Gdx.graphics.getHeight() / 2 - gap / 2 - bottomTube.getHeight() + tubeOffset[i], bottomTube.getWidth(), bottomTube.getHeight());
-
-            }
-
-            if (birdY > 0) {
-
-                // effect of gravity
-                velocity += gravity;
-                birdY -= velocity;
-            }
-            else {
-                // if bird falls below the screen, gameState to 2 to identify that the game is over
-                gameState = 2;
-            }
-        }
-        else if (gameState == 0) {
-
-            if (Gdx.input.justTouched()) {
-
-                // will begin the velocity and movement of the bird
-                gameState = 1;
-            }
-        } else if (gameState == 2) {
-
-            // display gameover in the middle of the screen
-            batch.draw(gameover, Gdx.graphics.getWidth() / 2 - gameover.getWidth() / 2, Gdx.graphics.getHeight() / 2 - gameover.getHeight() / 2);
-
-            // reset the game
-            if (Gdx.input.justTouched()) {
-
-                gameState = 1;
-                startGame();
-                score = 0;
-                scoringTube = 0;
-                velocity = 0;
-            }
-        }
-
-        if (flapState == 0) {
-            flapState = 1;
-        }
-        else {
-            flapState = 0;
-        }
-
-
-
-        // draw bird in the center of screen
-        batch.draw(birds[flapState], Gdx.graphics.getWidth() / 2 - birds[flapState].getWidth() / 2, birdY);
-
-        // bottom left draw score string
-        font.draw(batch, String.valueOf(score), 100, 200);
-
-        // end drawing sprites
-        batch.end();
-
-        // x coordinate of the bird, y coordinate, and radius of the bird
-        birdCircle.set(Gdx.graphics.getWidth() / 2, birdY + birds[flapState].getHeight() / 2, birds[flapState].getWidth() / 2);
-
-        // temporarily making rectangles that are covering tubes to be red to make sure it's covering them
-        for (int i = 0; i < numberOfTubes; i++) {
-
-            if (Intersector.overlaps(birdCircle, topTubeRectangles[i]) || Intersector.overlaps(birdCircle, bottomTubeRectangles[i])) {
-
-                Gdx.app.log("Collision", "Yes!");
-
-                gameState = 2;
-            }
-        }
 	}
-	
+
 	@Override
-	public void dispose () {
-//		batch.dispose();
-//		img.dispose();
+	public void render()
+	{
+
+	}
+
+	@Override
+	public void dispose()
+	{
+
 	}
 }
