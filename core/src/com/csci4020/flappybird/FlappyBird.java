31e4b14338e52524b45cf5faecb0695913353cdd
Brian Lake <bnlake@gmail.com>
2019-04-24 21:36:11 -0500

    Set up bitmaps

--- core/src/com/csci4020/flappybird/FlappyBird.java
+++ core/src/com/csci4020/flappybird/FlappyBird.java
@@ -2,13 +2,32 @@ package com.csci4020.flappybird;
 
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.graphics.Texture;
 
 public class FlappyBird extends ApplicationAdapter
 {
+	// Set up global variables
+	private Texture background;
+	private Texture[] birds;
+	private Texture gameover;
+	private Texture topTube;
+	private Texture bottomTube;
+
+	/**
+	 * This method is the first method to get called
+	 */
 	@Override
 	public void create()
 	{
+		// Set up the images as textures
+		background = new Texture("bg.png");
+		gameover = new Texture("gameover.png");
+		topTube = new Texture("toptube.png");
+		bottomTube = new Texture("bottomtube.png");
 
+		birds = new Texture[2];
+		birds[0] = new Texture("bird.png");
+		birds[1] = new Texture("bird2.png");
 	}
 
 	public void startGame()
@@ -16,6 +35,9 @@ public class FlappyBird extends ApplicationAdapter
 
 	}
 
+	/**
+	 * This method runs continuously
+	 */
 	@Override
 	public void render()
 	{
