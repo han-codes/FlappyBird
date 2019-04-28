58365db9f3ebe7174cbadbb64d7873261f433335
Brian Lake <bnlake@gmail.com>
2019-04-27 22:23:27 -0500

    Notes for next commits

--- core/src/com/csci4020/flappybird/FlappyBird.java
+++ core/src/com/csci4020/flappybird/FlappyBird.java
@@ -3,18 +3,13 @@ package com.csci4020.flappybird;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Color;
-import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Circle;
 import com.badlogic.gdx.math.Intersector;
 import com.badlogic.gdx.math.Rectangle;
 
 import java.util.Random;
-
-import org.w3c.dom.css.Rect;
 
 enum FlapStates
 {
